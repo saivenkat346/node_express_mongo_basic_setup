62f09e172be9a58e2e59b6c829c1d935a13662f2
saivenkat <saisam346@gmail.com>
2022-06-15 11:46:40 +0530

    server and database are initialized and connected

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express =require('express');
+const mongoose =require("mongoose");
 const app=express();
+
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost/Person",
+()=>{
+    console.log("database is connected");
+}
+);
+
 
 app.listen(3000,()=>{
    console.log("the server is connected to port 3000")
